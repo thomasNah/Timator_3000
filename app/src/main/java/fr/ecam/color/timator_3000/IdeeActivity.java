73f5b34f611bbad8432e6f3e1651646a37e61b32
Trickart <richard.sandeau.pro@gmail.com>
2020-04-01 16:56:53 +0200

    Erreur layout nouvelle activite (Viennot)

---
+++ app/src/main/java/fr/ecam/color/timator_3000/IdeeActivity.java
@@ -0,0 +1,14 @@
+package fr.ecam.color.timator_3000;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IdeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idee);
+    }
+}
--- app/src/main/java/fr/ecam/color/timator_3000/MainActivity.java
+++ app/src/main/java/fr/ecam/color/timator_3000/MainActivity.java
@@ -4,20 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    Spinner spinnerTempsDispo;
-    Button giveAnIdeaButton;
-    Button challengeLongTerme;
-    Button activitePersoButton;
-    Button preferencesButton;
+    private TextView tempsDispo;
+    private Spinner spinnerTempsDispo;
+    private Button giveAnIdeaButton;
+    private Button challengeLongTerme;
+    private Button activitePersoButton;
+    private Button preferencesButton;
+
+    String inputTempsDispo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //DECLARATION VARIABLE LAYOUT
-        TextView tempsDispo = findViewById(R.id.tempsDispo);
+        tempsDispo = findViewById(R.id.tempsDispo);
         spinnerTempsDispo = findViewById(R.id.spinnerTempsDispo);
         giveAnIdeaButton = findViewById(R.id.giveAnIdeaButton);
         challengeLongTerme = findViewById(R.id.challengeLongTermeButton);
@@ -37,6 +42,40 @@ public class MainActivity extends AppCompatActivity {
                 R.array.arrayTempsDispo, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerTempsDispo.setAdapter(adapter);
+
+        spinnerTempsDispo.setOnItemSelectedListener(this);
+
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+
+
+        giveAnIdeaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tempsDispo.setText(inputTempsDispo);
+
+                if(inputTempsDispo.equals("5 minutes")) {
+                    tempsDispo.setText("Crevard");
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        inputTempsDispo = parent.getItemAtPosition(position).toString();
+        //PERMET D'AFFICHER LE TEMPS DISPO EN PETITE CASE QUI DISPARAIT RAPIDEMENT
+        //Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
