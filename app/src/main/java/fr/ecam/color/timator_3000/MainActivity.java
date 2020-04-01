48518f80b8a14d509e2ac2e7d14759b2f40a54e1
Trickart <richard.sandeau.pro@gmail.com>
2020-04-01 15:54:28 +0200

    Lien avec layout fait

--- app/src/main/java/fr/ecam/color/timator_3000/MainActivity.java
+++ app/src/main/java/fr/ecam/color/timator_3000/MainActivity.java
@@ -3,12 +3,40 @@ package fr.ecam.color.timator_3000;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinnerTempsDispo;
+    Button giveAnIdeaButton;
+    Button challengeLongTerme;
+    Button activitePersoButton;
+    Button preferencesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //DECLARATION VARIABLE LAYOUT
+        TextView tempsDispo = findViewById(R.id.tempsDispo);
+        spinnerTempsDispo = findViewById(R.id.spinnerTempsDispo);
+        giveAnIdeaButton = findViewById(R.id.giveAnIdeaButton);
+        challengeLongTerme = findViewById(R.id.challengeLongTermeButton);
+        activitePersoButton = findViewById(R.id.activitePersoButton);
+        preferencesButton = findViewById(R.id.preferencesButton);
+
+        //PROPRE AU SPINNER (LISTE DEROULANTE POUR SELECTION DU TEMPS)
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.arrayTempsDispo, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTempsDispo.setAdapter(adapter);
+
     }
 }
