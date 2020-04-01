8be811b52d807ca5e83fab46b161960f1ea14028
Trickart <richard.sandeau.pro@gmail.com>
2020-04-01 17:30:17 +0200

    Nouvelle activité (idée)

--- app/src/main/java/fr/ecam/color/timator_3000/IdeeActivity.java
+++ app/src/main/java/fr/ecam/color/timator_3000/IdeeActivity.java
@@ -3,12 +3,26 @@ package fr.ecam.color.timator_3000;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class IdeeActivity extends AppCompatActivity {
+
+    TextView ideeText;
+    Button autreIdeeButton;
+    Button likeButton;
+    Button noLikeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idee);
+
+        ideeText = findViewById(R.id.ideeText);
+        autreIdeeButton = findViewById(R.id.autreIdeeButton);
+        likeButton = findViewById(R.id.likeButton);
+        noLikeButton = findViewById(R.id.noLikeButton);
+
+
     }
 }
