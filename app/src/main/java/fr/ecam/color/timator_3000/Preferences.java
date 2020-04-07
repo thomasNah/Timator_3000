b5981c459a7350619880337bc820d57344e1fb45
louis.villard <louis28villard@live.com>
2020-04-07 17:48:30 +0200

    ajout préférences

--- app/src/main/java/fr/ecam/color/timator_3000/Preferences.java
+++ app/src/main/java/fr/ecam/color/timator_3000/Preferences.java
@@ -2,9 +2,11 @@ package fr.ecam.color.timator_3000;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -14,33 +16,39 @@ public class Preferences extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             setTheme(R.style.DarkTheme);
-        }
-        else setTheme(R.style.AppTheme);
+        } else setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
-        modeSombre=findViewById(R.id.modeSombre);
-        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+        modeSombre = findViewById(R.id.modeSombre);
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             modeSombre.setChecked(true);
         }
         modeSombre.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
+                if (isChecked) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    restartApp();
-                }
-                else {
+                    recreate();
+                } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    restartApp();
+                    recreate();
                 }
             }
         });
+        //ADD BACK BUTTON POUR RETOURNER SUR MAIN ACTIVITY
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-    public void restartApp() {
-        Intent i = new Intent(getApplicationContext(),Preferences.class);
-        startActivity(i);
-        finish();
+    @Override
+    public boolean onOptionsItemSelected (@NonNull MenuItem item){
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            Intent mainActivity = new Intent(Preferences.this, MainActivity.class);
+            startActivity(mainActivity);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
