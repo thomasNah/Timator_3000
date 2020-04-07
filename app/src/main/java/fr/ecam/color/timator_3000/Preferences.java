abd59f6482b966d6c03c9f0151a97aa636c2b667
louis.villard <louis28villard@live.com>
2020-04-07 12:15:22 +0200

    ajout préférences

--- app/src/main/java/fr/ecam/color/timator_3000/Preferences.java
+++ app/src/main/java/fr/ecam/color/timator_3000/Preferences.java
@@ -1,15 +1,46 @@
 package fr.ecam.color.timator_3000;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class Preferences extends AppCompatActivity {
 
+    private Switch modeSombre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        }
+        else setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+        modeSombre=findViewById(R.id.modeSombre);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            modeSombre.setChecked(true);
+        }
+        modeSombre.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
+    }
+    public void restartApp() {
+        Intent i = new Intent(getApplicationContext(),Preferences.class);
+        startActivity(i);
+        finish();
     }
 }
