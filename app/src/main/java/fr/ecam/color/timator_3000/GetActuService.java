e47dfea3c53170d40f83ca443df35481ffd37caa
louis.villard <louis28villard@live.com>
2020-05-01 15:29:27 +0200

    API actualité

--- app/src/main/java/fr/ecam/color/timator_3000/GetActuService.java
+++ app/src/main/java/fr/ecam/color/timator_3000/GetActuService.java
@@ -1,12 +1,12 @@
 package fr.ecam.color.timator_3000;
 
+import fr.ecam.color.timator_3000.model.Actu;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface GetActuService {
-    @GET("top-headlines?country=fr&apiKey=e5b6ccfeee244e3fa804e4fc2809ff58")
-    Call<ActuResult> getArticle();
+    @GET("top-headlines")
+    Call<Actu> getArticle(@Query("country") String country, @Query("key") String key);
 }
 
--- app/src/main/java/fr/ecam/color/timator_3000/MainActivity.java
+++ app/src/main/java/fr/ecam/color/timator_3000/MainActivity.java
@@ -19,6 +19,8 @@ import androidx.appcompat.app.AppCompatDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
+import fr.ecam.color.timator_3000.model.Actu;
+import fr.ecam.color.timator_3000.model.Article;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -126,22 +128,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     startActivity(WeatherActivityInt);
                 } else if (inputTempsDispo.equals("2 minutes")) {
                     GetActuService ws = RetrofitBuilder.getSimpleClient();
-                    ws.getArticle().enqueue(new Callback<ActuResult>() {
+                    ws.getArticle("fr","e5b6ccfeee244e3fa804e4fc2809ff58").enqueue(new Callback<Actu>() {
                         @Override
-                        public void onResponse(Call<ActuResult> call, Response<ActuResult> response) {
+                        public void onResponse(Call<Actu> call, Response<Actu> response) {
                             if (response.code()==200) {
-                                System.out.println("onResponse " + response.body().getName());
-                                System.out.println("onResponse " + response.body().getTitle());
-                                System.out.println("onResponse " + response.body().getDescription());
-                                System.out.println("onResponse " + response.body().getHtmlUrl());
-                                System.out.println("onResponse " + response.body().getPublishedAt());
+                                System.out.println(response.body().getArticles().size());
+                                //Récupération du 1er article
+                                Article article = response.body().getArticles().get(0);
+                                System.out.println("onResponse " + article.getTitle());
+                                System.out.println("onResponse " + article.getDescription());
+                                System.out.println("onResponse " + article.getPublishedAt());
                             } else {
                                 System.out.println("Une erreur est survenue " + response.code());
                             }
                             System.out.println(response.body().toString());
                         }
                         @Override
-                        public void onFailure(Call<ActuResult> call, Throwable t) { System.out.println("onFailure " + t.getMessage());
+                        public void onFailure(Call<Actu> call, Throwable t) {
+                            System.out.println("onFailure " + t.getMessage());
                         }
                     });
                     /*Intent GetActuServiceActivity = new Intent(MainActivity.this, GetActuService.class);
